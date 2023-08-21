7af579bf511f5046eb9928ca10e02eb69ea08b50
eldano6189 <eldano6189@gmail.com>
2023-08-21 19:29:01 +0300

    more bug fixes

--- src/pages/contact-page/components/form/form.jsx
+++ src/pages/contact-page/components/form/form.jsx
@@ -5,7 +5,7 @@ const Form = () => {
     <div className={styles.container}>
       <div className={styles.container__inner}>
         <form className={styles.form} name="contact" method="post">
-          <input type="hidden" name="contact" value="contact" />
+          <input type="hidden" name="form-name" value="contact" />
           <div className={styles.form__input}>
             <div className={styles.wrapper__input}>
               <p>Email*</p>
